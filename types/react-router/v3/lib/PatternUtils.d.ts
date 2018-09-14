40fa4a9fa98999282f573b5649c53df0fc54abdf
Christian Gill <gillchristiang@gmail.com>
2018-09-14 11:09:31 +0200

    Update react-router v3 PatternUtils
    Add types for `matchPattern`, exported from react-router/lib/PatternUtils

--- types/react-router/v3/lib/PatternUtils.d.ts
+++ types/react-router/v3/lib/PatternUtils.d.ts
@@ -1,3 +1,11 @@
 import { RoutePattern } from "react-router";
 
 export function formatPattern(pattern: RoutePattern, params: any): string;
+
+interface MatchedPattern {
+  remainingPathname: string;
+  paramNames: Array<string>;
+  paramValues: Array<string>;
+}
+
+export function matchPattern(pattern: string, pathname: string): MatchedPattern | null;
